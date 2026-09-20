04f72ddfbdeaef76c8457b641b3f2e1dea40f6f6
Mark Arneman <markarneman@gmail.com>
2026-09-19 21:37:48 -0400

    page cleanup

--- website/build.sh
+++ website/build.sh
@@ -7,13 +7,9 @@ readonly ROOT
 readonly OUT="${ROOT}/_site"
 
 main() {
-  # SITE_REPO is the public GitHub repo holding the releases; local builds get a dead link.
-  local releases="${SITE_REPO:+https://github.com/${SITE_REPO}/releases/latest}"
-
   rm -rf "$OUT"
   mkdir -p "$OUT"
   cp "${ROOT}"/website/*.{html,css,png} "$OUT"/
-  sed -i "s|{{RELEASES}}|${releases:-#}|g" "${OUT}/index.html"
 }
 
 main "$@"
